f28b5dcaed5d5fbb9cc7e42eeab475e7bb4dacb3
krishna <arjunankrishnakumar1@gmail.com>
2018-06-23 21:25:01 +0530

    Adding authguard for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CandidateService } from './services/candidate.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 @NgModule({
     imports: [
@@ -18,7 +18,7 @@ import { AppComponent } from './app.component';
         AppRoutingModule,
     ],
     declarations: [AppComponent],
-    providers: [CandidateService],
+    providers: [AuthGuard],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
