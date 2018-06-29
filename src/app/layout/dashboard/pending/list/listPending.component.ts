0de2e852da8b57592791374e59f5c954070eebba
Amit Kumar <amit8.88@gmail.com>
2018-06-29 18:55:24 +0530

    chore: commentgetpending interviews api call till its implemented.

--- src/app/layout/dashboard/pending/list/listPending.component.ts
+++ src/app/layout/dashboard/pending/list/listPending.component.ts
@@ -1,20 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IInterviewer } from '../../../../services/IInterviewer';
-import { ICandidate } from '../../../../services/ICandidate';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IPending } from '../../../../services/IPending';
+import { IPendingService } from '../../../../services/IPendingService';
+
+
 
 
 @Component({
     selector: 'list-pending',
     templateUrl: './listPending.component.html',
-    styleUrls: ['./listPending.component.scss']
+    styleUrls: ['./listPending.component.scss'],
+    // animations: [routerTransition()]
 })
-
 export class ListPendingComponent implements OnInit {
-    
-    @Input('icandidateList') icandidate: ICandidate[];
-    
-    ngOnInit() {
-        console.log("Candidate List : " + JSON.stringify(this.icandidate));
+    errorMessage: String;
+    ipendingList: IPending[];
+    ipending: IPending;
+    listPendingFilter: String = '';
+    list : any [] = new  Array();
+    conString : String = '';
+
+    filteredIpending: IPending[];
+
+   constructor(  private router: Router,
+                private _ipendingService: IPendingService) {
+
+   }
+
+    ngOnInit():void {
+        this.load();
+    }
+
+    private load() {
+        /*this._ipendingService.getPendingInterviews()
+                .subscribe(data => {
+                    this.ipendingList = data,
+                    error => this.errorMessage = <any>error;
+                 }
+
+                );*/
+    }
+
+
+    private onSaveComplete(): void {
+        this.load();
+
     }
 
 }
